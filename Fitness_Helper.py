609affef30913b55c2578821216bc18be453b26b
Soumya Agarwal <soumyaagarwala@gmail.com>
2023-03-27 16:52:52 +0530

    add code for another hand and removed unwanted code

--- Fitness_Helper.py
+++ Fitness_Helper.py
@@ -9,11 +9,13 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 detector = pD.poseDetector()       #creating object
 pTime = 0
-dir = 0     #direction 
-count = 0 
+flag1 = 0      
+flag2 = 0     
+count1 = 0 
+count2 = 0 
 
-while True:     
-    # cap = cv2.VideoCapture('Pose_Estimation_Work/human_face.jpg')
+
+while True:  
     success, img = cap.read()
     # img = cv2.resize(img, (640, 480))
     # img = cv2.resize(img, (1280, 720))
@@ -23,37 +25,59 @@ while True:
     if lmList != None and len(lmList) !=0 :
         rightHand = detector.findAngle(img, 12,14,16)     
         leftHand = detector.findAngle(img, 11,13,15)
-        #this 25,165 are by observing(for now considering dumble exercice and by sample running fetch its max and min)
-        # perCent = np.interp(rightHand, (22,165),(0,100))       # (changable value, changeable val btw this, convert_in_this_range)
-        perCent = np.interp(rightHand, (25,165),(100,0))       # (changable value, changeable val btw this, convert_in_this_range)
-        # print(rightHand,perCent)
 
-        colorCode = (77, 255, 147)      # this is in RGB since (OpenCV reads in images in BGR format (instead of RGB) because when OpenCV was first being developed, BGR color format was popular among camera manufacturers and image software providers.)
-        if perCent == 100:
-            colorCode = (0, 255, 0)
-            if dir == 0:
-                count += 0.5
-                dir = 1
-        if perCent == 0:
-            if dir == 1:
-                count += 0.5
-                dir = 0
+        # leftLeg = detector.findAngle(img, 23,25,27)
+        # rightLeg = detector.findAngle(img, 24,26,28)
+    
+        perCent1 = np.interp(rightHand, (25,165),(100,0))       # (changable value, changeable val btw this, convert_in_this_range)
+        perCent2 = np.interp(leftHand, (25,165),(100,0))     
+        
+#left
+        colorCode1 = (77, 255, 147)      # this is in BGR since openCV uses BGR format
+        if perCent1 == 100:
+            colorCode1 = (0, 255, 0)
+            if flag1 == 0:
+                count1 += 0.5
+                flag1 = 1
+        if perCent1 == 0:
+            if flag1 == 1:
+                count1 += 0.5
+                flag1 = 0
+        # print(count)
+
+#right
+        colorCode2 = (77, 255, 147)      # this is in BGR since openCV uses BGR format
+        if perCent2 == 100:
+            colorCode2 = (0, 255, 0)
+            if flag2 == 0:
+                count2 += 0.5
+                flag2 = 1
+        if perCent2 == 0:
+            if flag2 == 1:
+                count2 += 0.5
+                flag2 = 0
         # print(count)
         
     #bar
-    bar = np.interp(rightHand, (25,165),(60, img.shape[1]-240))       # (changable value, changeable val btw this, convert_in_this_range)
+    bar1 = np.interp(rightHand, (25,165),(110, img.shape[1]-240)) 
+    bar2 = np.interp(leftHand, (25,165),(110, img.shape[1]-240)) 
     # print(bar)
-    cv2.rectangle(img, (img.shape[0]+130, 60), (img.shape[0]+90, img.shape[1]-240), colorCode, 3)
-    cv2.rectangle(img, (img.shape[0]+130, int(bar)), (img.shape[0]+90, img.shape[1]-240), colorCode, cv2.FILLED)
-    cv2.putText(img, f"{str(int(perCent))}%", (img.shape[0]+75,40), cv2.FONT_HERSHEY_PLAIN, 2, colorCode, 3)  
+    #right side 
+    cv2.rectangle(img, (img.shape[0]+140, 110), (img.shape[0]+110, img.shape[1]-240), colorCode2, 3)
+    cv2.rectangle(img, (img.shape[0]+140, int(bar2)), (img.shape[0]+110, img.shape[1]-240), colorCode2, cv2.FILLED)
+    cv2.putText(img, f"{str(int(perCent2))}%", (img.shape[0]+100,90), cv2.FONT_HERSHEY_PLAIN, 2, colorCode2, 2) 
+    cv2.putText(img, f"Count:{str(int(count2))}", (img.shape[1]-140,50), cv2.FONT_ITALIC, 1, (0,255,0), 2)  
+    #left side 
+    cv2.rectangle(img, (15, 110), (45, img.shape[1]-240), colorCode1, 3)
+    cv2.rectangle(img, (15, int(bar1)), (45, img.shape[1]-240), colorCode1, cv2.FILLED)
+    cv2.putText(img, f"{str(int(perCent1))}%", (15,90), cv2.FONT_HERSHEY_PLAIN, 2, colorCode1, 2)
+    cv2.putText(img, f"Count:{str(int(count1))}", (10,50), cv2.FONT_ITALIC, 1, (0,255,0), 2)  
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     # to show FPS(frame per second) 
-    cv2.putText(img, f"FPS:{str(int(fps))}", (40,90), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 3)  
-    # to show counts completed 
-    cv2.putText(img, f"Count:{str(int(count))}", (40,50), cv2.FONT_ITALIC, 1, (0,255,0), 3)  
+    cv2.putText(img, f"FPS:{str(int(fps))}", (int((img.shape[1])/2 - 20),90), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 3)  
     cv2.imshow("Image Name", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break   
--- pose_Detector.py
+++ pose_Detector.py
@@ -1,3 +1,5 @@
+# as module
+
 import cv2
 import mediapipe as mp 
 import time
@@ -7,36 +9,23 @@ import math
 class poseDetector():
 
     def __init__(self):
-    # def __init__(self, mode=False, complexity=1, smoothLm=True, enableSeg=False, smoothSeg=True, detectionCon=0.5, trackCon=0.5):
-    #     self.mode=mode
-    #     self.complexity=complexity
-    #     self.smoothLm=smoothLm
-    #     self.enableSeg=enableSeg
-    #     self.smoothSeg=smoothSeg
-    #     self.detectionCon=detectionCon
-    #     self.trackCon=trackCon
 
         self.mp_drawing_styles = mp.solutions.drawing_styles     
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose()        #creating pose
-        # self.pose = self.mpPose.Pose(self.mode, self.complexity, self.smoothLm, self.enableSeg, self.smoothSeg, self.detectionCon, self.trackCon)        #creating pose
-
+       
     def findPerson(self, img, draw=False):
 
-        # this img is in BGR and mediapipe uses RGB therefore...we convert it into RGB
+        # this img is in BGR therefore...we convert it into RGB
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
-        # print(results.pose_landmarks)
+        # print(self.results.pose_landmarks)
 
         if self.results.pose_landmarks:      # landmarks are there AND
-            if draw:         # if landmarks to be drawn 
-                
+            if draw:        
                 # Draw pose landmarks on the image. (0 to 32 points are there)
-
-                # self.mpDraw.draw_landmarks(img, self.results.pose_landmarks)  # it make all the points
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,self.mpPose.POSE_CONNECTIONS)  # it connect all the points
-                # self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,self.mpPose.POSE_CONNECTIONS, landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())  # it make all the points color different
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,self.mpPose.POSE_CONNECTIONS, landmark_drawing_spec= self.mp_drawing_styles.get_default_pose_landmarks_style())  # (img, make all the points, connect all the points, make all the points color different)            
         # else:
         #     print('no body is shown...')
 
@@ -52,7 +41,7 @@ class poseDetector():
 
                 # to get actual pixel values of lm
                 cx, cy = int(lm.x*w) , int(lm.y*h)
-                self.lmList.append([id,cx,cy])      # we can also append z value | visiblity value...but for now we do not need it.
+                self.lmList.append([id,cx,cy])      
 
                 if draw:
                     #drawing circle at landmark points( just for checking)
@@ -63,23 +52,16 @@ class poseDetector():
 
     def findAngle(self, img, p1, p2, p3, draw= True, showAngle= False):
 
-        # getting targeted landmarks x,y values
+        # getting targeted landmarks x,y coordinates values
         p1_x, p1_y = self.lmList[p1][1:]        # OR lmList[p1][1], lmList[p1][2])
         p2_x, p2_y = self.lmList[p2][1:]
         p3_x, p3_y = self.lmList[p3][1:]
         
         #finding angle
-        angle = math.degrees(math.atan2(p3_y - p2_y, p3_x - p2_x) - math.atan2(p1_y - p2_y, p1_x - p2_x))   # math.degrees() will convert given radian value to degree | atan( returns value in range -PI/2 to PI/2 radians. atan2() returns in range -PI to PI)
-        # if angle < 0:
-        #     angle += 360
+        angle = math.degrees(math.atan2(p3_y - p2_y, p3_x - p2_x) - math.atan2(p1_y - p2_y, p1_x - p2_x))   # math.degrees() will convert radian value to degree
+        
         angle = (angle + 180) % 360 - 180
         angle = abs(angle)
-        # if angle < 0:
-        #     angle = abs(angle)
-        # if angle > 180:
-        #     angle -= 180
-        # print(angle)
-
         
         # drawing and joining targeted points
         if draw:
@@ -95,40 +77,7 @@ class poseDetector():
             cv2.circle(img, (p3_x,p3_y), 12, (0,0,255), 2)
 
         if showAngle:
-            #put angle value
-            # I think both work fine...for showing angle
             cv2.putText(img, str(int(angle)), (p2_x+5, p2_y+5), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2)  
-            # cv2.putText(img, str(int(abs(angle))), (p2_x+5, p2_y+5), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,0), 2)   #adding 360 if -ve value 
 
         return angle
     
-    
-# to make it a module we write like below...
-def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-    detector = poseDetector()       #creating object
-
-    while True:     
-        success, img = cap.read()
-
-        img = detector.findPerson(img,True)        #calling method 
-        lmList = detector.findPosition(img,True)     # since if we did not call findPerson method prior to this, then we can not access result attribute(variable)(...i think for this we can check in findPositio method if result is present then OK else call findPerson inside findPosition method)
-        # print("32 LandMarks list: ",lmList)
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        # put text in the frame 
-        cv2.putText(img, str(int(fps)), (40,50), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 3)  
-
-        cv2.imshow("Image Name", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cv2.destroyAllWindows()
-
-
-#TO RUN THIS FILE UNCOMMENT BELOW LINE
-# if __name__ == "__main__":
-#     main()      # if we run this file then it will run this main function...else if we are calling another function...then it will not run the if part
